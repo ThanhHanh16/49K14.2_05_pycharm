5f7e791d031bb41bddc772415c9d2b9707c53a2d
ThanhHanh16 <thanhhanh0104@gmail.com>
2026-04-04 00:02:52 +0700

    test

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    full_name = models.CharField(max_length=100, verbose_name="Họ và tên")
+    phone_number = models.CharField(max_length=20, unique=True, verbose_name="Số điện thoại")
+    email = models.EmailField(blank=True, null=True, verbose_name="Email")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Customer
+from .serializers import CustomerSerializer
 
-# Create your views here.
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all().order_by('-created_at')
+    serializer_class = CustomerSerializer
+
+    # Cấu hình tính năng tìm kiếm khách hàng
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['full_name', 'phone_number']  # Tìm kiếm theo Tên hoặc SĐT
