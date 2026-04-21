d221d63e2ea28c0afe776bc190622ae2638d1c17
Tran Le Phuong Linh <linhnu.2185@gmail.com>
2026-04-21 18:54:06 +0700

    ...

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import (
     Customer, CourtType, Court, PriceTable, PriceTableCourt,
-    PriceTableTimeSlot, Booking
+    PriceTableTimeSlot, Booking, QLDonDat
 )
 
 
@@ -59,4 +59,13 @@ class BookingAdmin(admin.ModelAdmin):
     search_fields = ('customer_name', 'phone', 'court__name')
     list_filter = ('status', 'date', 'court__court_type')
     ordering = ('-date', '-created_at')
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(QLDonDat)
+class QLDonDatAdmin(admin.ModelAdmin):
+    list_display = ('booking_code', 'ten_khach_hang', 'so_dien_thoai', 'ngay_dat', 'trang_thai_don', 'thanh_toan')
+    search_fields = ('booking_code', 'ten_khach_hang', 'so_dien_thoai')
+    list_filter = ('trang_thai_don', 'thanh_toan', 'ngay_dat')
+    ordering = ('-ngay_dat', '-created_at')
     readonly_fields = ('created_at', 'updated_at')
